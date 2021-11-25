8584e24f386628ab868fe7a2d0682f66bc76b3d4
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2021-11-25 22:26:10 +0100

    update

--- java/spring/spring-microservices/level-3 Configuration/spring-boot-configuration/src/main/java/com/greydev/springbootconfiguration/GreetingController.java
+++ java/spring/spring-microservices/level-3 Configuration/spring-boot-configuration/src/main/java/com/greydev/springbootconfiguration/GreetingController.java
@@ -1,5 +1,8 @@
 package com.greydev.springbootconfiguration;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +11,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${app.fullName}")
-	private String name;
+	@Value("${app.fullName: default value}")
+	private String fullName;
+
+	@Value("${app.list}")
+	private List<String> list;
+
+	@Value("${app.array}")
+	private String[] array;
+
+	@Value("#{${app.mapString}}")
+	private Map<String, String> mapString;
+
+	@Value("#{${app.mapInt}}")
+	private Map<String, Integer> mapInt;
+
+	@Value("#{${app.mapDouble}}")
+	private Map<String, Double> mapDouble;
+
+	@Value("#{${app.mapMap}}")
+	private Map<String, Map<String, String>> mapMap;
+
 
 	@GetMapping("/greeting")
 	public String greeting() {
-		return "Hello " + name;
+
+		return "<p>Hello " + fullName
+				+ "<br> list: " + list + ", size: " + list.size()
+				+ "<br> array: " + array + ", size: " + array.length
+				+ "<br> mapString: " + mapString + ", size: " + mapString.size()
+				+ "<br> mapInt: " + mapInt + ", size: " + mapInt.size()
+				+ "<br> mapDouble: " + mapDouble + ", size: " + mapDouble.size()
+				+ "<br> mapMap: " + mapMap + ", size: " + mapMap.size()
+				+ "</p>";
 	}
 }
 
