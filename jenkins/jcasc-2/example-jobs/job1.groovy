d349ec589e3b8ef09dee00bcea652a53b8b19843
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2022-04-15 19:02:22 +0200

    update jenkins

--- jenkins/jcasc-2/example-jobs/job1.groovy
+++ jenkins/jcasc-2/example-jobs/job1.groovy
@@ -57,16 +57,15 @@ pipelineJob('job-name') {
       script('''
         pipeline {
             agent any
-                stages {
-                    stage('Stage 1') {
-                        steps {
-                            echo 'logic'
-                        }
+            stages {
+                stage('Stage 1') {
+                    steps {
+                        echo 'logic'
                     }
-                    stage('Stage 2') {
-                        steps {
-                            echo 'logic'
-                        }
+                }
+                stage('Stage 2') {
+                    steps {
+                        echo 'logic'
                     }
                 }
             }
