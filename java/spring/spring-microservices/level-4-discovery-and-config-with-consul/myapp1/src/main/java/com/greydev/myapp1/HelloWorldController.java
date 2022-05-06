6d528143d91c5bb250b4e3f545ca09559698334c
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2022-05-06 18:35:29 +0200

    adjust consul example class

--- java/spring/spring-microservices/level-4-discovery-and-config-with-consul/myapp1/src/main/java/com/greydev/myapp1/HelloWorldController.java
+++ java/spring/spring-microservices/level-4-discovery-and-config-with-consul/myapp1/src/main/java/com/greydev/myapp1/HelloWorldController.java
@@ -3,13 +3,14 @@ package com.greydev.myapp1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
 @RestController
-@RefreshScope
+@RefreshScope // this is needed so @Value("${app.externalProp1}") updates dynamically
 public class HelloWorldController {
 
 	@Autowired
@@ -19,9 +20,18 @@ public class HelloWorldController {
 	@Value("${app.externalProp1}")
 	private String externalProp1;
 
+	// You can also dynamically get the environment property from consul
+	// The changes are reflected immediately since env.getProperty will be called each time and fetches the newest value.
+	// AFAIK these values could be created in consul by hand or with a REST api
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/hello")
 	public String helloWorld() {
-		return "hello world from myapp 1";
+		return String.format("hello world from myapp 1. <br>" +
+				"app.dynamicProperty: %s <br>" +
+				"app.externalProperty1: %s",
+				env.getProperty("app.dynamicProperty"), externalProp1);
 	}
 
 
@@ -35,11 +45,6 @@ public class HelloWorldController {
 	public String callMyapp3() {
 		return "Response from myapp3: "
 				+ this.restTemplate.getForObject("http://myapp3/hello", String.class);
-	}
-
-	@GetMapping("/external")
-	public String external() {
-		return "Value of external prop: " + externalProp1;
 	}
 
 }
