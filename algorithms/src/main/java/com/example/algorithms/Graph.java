9e4a91f74cdd744bf12b8893f5f555bd657e1b39
Jonathan-Can Kleiner <canozbek1@yahoo.com>
2022-06-03 11:39:52 +0200

    dfs and bfs algorithms

--- algorithms/src/main/java/com/example/algorithms/Graph.java
+++ algorithms/src/main/java/com/example/algorithms/Graph.java
@@ -5,63 +5,148 @@ import java.util.*;
 
 public class Graph {
 
-	public static List<String> breadthFirstPrint(Map<String, List<String>> graph) {
+	public static List<String> breadthFirstPrint(Map<String, List<String>> graph, String startNode) {
 		// queue
 		Queue<String> q = new LinkedList<>();
-		List<String> nodesVisited = new ArrayList<>();
+		// instant lookup is important.
+		// If we use a list, we need to traverse the list each time to find if a node was visited
+		Map<String, Boolean> nodesVisited = new HashMap<>();
+		// used only for testing
+		List<String> nodesVisitedOrdered = new ArrayList<>();
 
 		// start node -> A
-		q.add("A");
+		q.add(startNode);
 
 		while (!q.isEmpty()) {
 			// pop the first node in the queue
 			String current = q.poll();
-			nodesVisited.add(current);
+
+			if (nodesVisited.get(current) != null) {
+				continue;
+			}
+
+			nodesVisited.put(current, true);
+			nodesVisitedOrdered.add(current);
 			System.out.println(current);
-			// add all the nodes neighbours to the queue
-			List<String> neighbours = graph.get(current);
-			q.addAll(neighbours);
+
+			// add all the neighbours to the queue
+			q.addAll(graph.get(current));
 		}
 
-		return nodesVisited;
+		return nodesVisitedOrdered;
 	}
 
 
-	public static List<String> depthFirstPrint(Map<String, List<String>> graph) {
+	public static List<String> depthFirstPrint(Map<String, List<String>> graph, String startNode) {
 		// stack
 		Stack<String> stack = new Stack<>();
-		List<String> nodesVisited = new ArrayList<>();
+		// instant lookup is important.
+		// If we use a list, we need to traverse the list each time to find if a node was visited
+		Map<String, Boolean> nodesVisited = new HashMap<>();
+		// used only for testing
+		List<String> nodesVisitedOrdered = new ArrayList<>();
 
 		// start node -> A
-		stack.add("A");
+		stack.add(startNode);
 
 		while (!stack.isEmpty()) {
 			String current = stack.pop();
-			nodesVisited.add(current);
+
+			// if node was visited before, skip the iteration
+			if (nodesVisited.get(current) != null) {
+				continue;
+			}
+
+			nodesVisited.put(current, true);
+			nodesVisitedOrdered.add(current);
 			System.out.println(current);
-			List<String> neighbours = graph.get(current);
-			stack.addAll(neighbours);
+
+			// add the neighbours to the stack
+			stack.addAll(graph.get(current));
 		}
 
 		System.out.println();
-		return nodesVisited;
+		return nodesVisitedOrdered;
 	}
 
 
-	public static List<String> depthFirstPrintRecursive(Map<String, List<String>> graph) {
-		// stack
-		Stack<String> stack = new Stack<>();
+	public static boolean hasPathBfs(Map<String, List<String>> graph, String srcNode, String targetNode) {
+		Queue<String> q = new LinkedList<>();
+		Map<String, Boolean> visitedNodes = new HashMap<>();
+		q.add(srcNode);
 
-		// start node -> A
-		stack.add("A");
+		while (!q.isEmpty()) {
+			String current = q.poll();
 
-		while (!stack.isEmpty()) {
-			String current = stack.pop();
-			System.out.println(current);
-			List<String> neighbours = graph.get(current);
-			stack.addAll(neighbours);
+			// if node was already visited
+			if (visitedNodes.get(current) != null) {
+				continue;
+			}
+
+			if (targetNode.equals(current)) {
+				return true;
+			}
+
+			q.addAll(graph.get(current));
+			visitedNodes.put(current, true);
 		}
-		System.out.println();
-		return null;
+
+		return false;
+	}
+
+
+	public static boolean hasPathDfsRecursive(Map<String, List<String>> graph,
+			String srcNode,
+			String targetNode,
+			Map<String, Boolean> nodesWithPathToTarget) {
+
+		if (nodesWithPathToTarget == null) {
+			nodesWithPathToTarget = new HashMap<>();
+		}
+
+		if (srcNode.equals(targetNode)) {
+			return true;
+		}
+
+		for (String neighbour : graph.get(srcNode)) {
+			boolean hasNeighbourPath = hasPathDfsRecursive(graph, neighbour, targetNode, nodesWithPathToTarget);
+			nodesWithPathToTarget.put(srcNode, hasNeighbourPath);
+
+			if (hasNeighbourPath) {
+				return true;
+			}
+		}
+
+		// if node has no neighbours
+		if (graph.get(srcNode).isEmpty()) {
+			nodesWithPathToTarget.put(srcNode, false);
+		}
+
+		return nodesWithPathToTarget.get(srcNode);
+	}
+
+
+	// the call stack can be used as our stack to go deeper
+	public static void depthFirstPrintRecursive(Map<String, List<String>> graph, String node, Map<String, Boolean> visitedNodes) {
+		// this function does not have an explicit base case
+		// the implicit base case is a node with no neighbours
+
+		if (visitedNodes == null) {
+			visitedNodes = new HashMap<>();
+		}
+
+		if (visitedNodes.get(node) != null) {
+			// node already visited
+			return;
+		}
+
+		System.out.println(node);
+
+		List<String> neighbours = graph.get(node);
+		visitedNodes.put(node, true);
+
+		for (String n : neighbours) {
+			depthFirstPrintRecursive(graph, n, visitedNodes);
+		}
 	}
 }
--- algorithms/src/test/java/com/example/algorithms/GraphTest.java
+++ algorithms/src/test/java/com/example/algorithms/GraphTest.java
@@ -11,19 +11,68 @@ class GraphTest {
 
 	@Test
 	void testBreadthFirstPrint() {
-		assertThat(Graph.breadthFirstPrint(buildGraph()))
+		assertThat(Graph.breadthFirstPrint(buildAcyclicGraph(), "A"))
+				.isEqualTo(Arrays.asList("A", "B", "C", "D", "E", "F"));
+
+		assertThat(Graph.breadthFirstPrint(buildCyclicGraph(), "A"))
 				.isEqualTo(Arrays.asList("A", "B", "C", "D", "E", "F"));
 	}
 
 
 	@Test
 	void testDepthFirstPrint() {
-		assertThat(Graph.depthFirstPrint(buildGraph()))
+		assertThat(Graph.depthFirstPrint(buildAcyclicGraph(), "A"))
+				.isEqualTo(Arrays.asList("A", "C", "E", "B", "D", "F"));
+
+		assertThat(Graph.depthFirstPrint(buildCyclicGraph(), "A"))
 				.isEqualTo(Arrays.asList("A", "C", "E", "B", "D", "F"));
 	}
 
 
-	Map<String, List<String>> buildGraph() {
+	@Test
+	void testHasPathBfs() {
+		assertThat(Graph.hasPathBfs(buildAcyclicGraph(), "A", "F")).isTrue();
+
+		assertThat(Graph.hasPathBfs(buildCyclicGraph(), "A", "F")).isTrue();
+
+		assertThat(Graph.hasPathBfs(buildGraph3(), "F", "J")).isFalse();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "F", "H")).isTrue();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "F", "K")).isTrue();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "F", "I")).isTrue();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "F", "G")).isTrue();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "J", "H")).isTrue();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "J", "F")).isFalse();
+		assertThat(Graph.hasPathBfs(buildGraph3(), "J", "J")).isTrue();
+	}
+
+
+	@Test
+	void testDepthFirstPrintRecursive() {
+		Graph.depthFirstPrintRecursive(buildAcyclicGraph(), "A", null);
+		Graph.depthFirstPrintRecursive(buildCyclicGraph(), "A", null);
+		System.out.println();
+	}
+
+
+	@Test
+	void testHasPathDfsRecursive() {
+		assertThat(Graph.hasPathDfsRecursive(buildAcyclicGraph(), "A", "F", null)).isTrue();
+
+		assertThat(Graph.hasPathDfsRecursive(buildCyclicGraph(), "A", "F", null)).isTrue();
+
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "F", "J", null)).isFalse();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "F", "H", null)).isTrue();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "F", "K", null)).isTrue();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "F", "I", null)).isTrue();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "F", "G", null)).isTrue();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "J", "H", null)).isTrue();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "J", "F", null)).isFalse();
+		assertThat(Graph.hasPathDfsRecursive(buildGraph3(), "J", "J", null)).isTrue();
+		System.out.println();
+	}
+
+
+	Map<String, List<String>> buildAcyclicGraph() {
 		Map<String, List<String>> map = new HashMap<>();
 		map.put("A", Arrays.asList("B", "C"));
 		map.put("B", Arrays.asList("D"));
@@ -31,6 +80,32 @@ class GraphTest {
 		map.put("D", Arrays.asList("F"));
 		map.put("E", Collections.emptyList());
 		map.put("F", Collections.emptyList());
+		return map;
+	}
+
+
+	Map<String, List<String>> buildCyclicGraph() {
+		Map<String, List<String>> map = new HashMap<>();
+		map.put("A", Arrays.asList("B", "C"));
+		map.put("B", Arrays.asList("D"));
+		map.put("C", Arrays.asList("E"));
+		map.put("D", Arrays.asList("F"));
+		map.put("E", Collections.emptyList());
+		// F goes back to A, if the code does not track the visited nodes
+		// it will go into an infinite loop
+		map.put("F", Arrays.asList("A"));
+		return map;
+	}
+
+
+	Map<String, List<String>> buildGraph3() {
+		Map<String, List<String>> map = new HashMap<>();
+		map.put("F", Arrays.asList("G", "I"));
+		map.put("G", Arrays.asList("H"));
+		map.put("H", Collections.emptyList());
+		map.put("I", Arrays.asList("G", "K"));
+		map.put("J", Arrays.asList("I"));
+		map.put("K", Collections.emptyList());
 		return map;
 	}
 
